d6d4d63661f5b42977e12966028f101aef1a5eb7
Guillermo Roman <gromande@hawk.iit.edu>
2020-04-12 20:40:14 -0500

    Adding DynamoDB table

--- src/app.py
+++ src/app.py
@@ -1,42 +1,63 @@
-import json
+import json, boto3
 
-# import requests
+KEY_NAME = 'message_id'
+KEY_VALUE = 'greeting'
+ATTR_NAME = 'message'
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('greetingTable')
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+def get_lambda_handler(event, context):
+    response = table.get_item(
+        Key={
+            KEY_NAME: KEY_VALUE
+        }
+    )
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    message = "Hello World!"
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    if 'Item' in response:
+        message = response['Item'][ATTR_NAME]
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+            ATTR_NAME: message
+        })
+    }
+
+def post_lambda_handler(event, context):
+
+    response = table.get_item(
+        Key={
+            KEY_NAME: KEY_VALUE
+        }
+    )
+
+    body = json.loads(event['body'])
+
+    message = body[ATTR_NAME]
+
+    if 'Item' in response:
+        table.update_item(
+            Key={
+                KEY_NAME: KEY_VALUE
+            },
+            AttributeUpdates={
+                ATTR_NAME: {'Value': message}
+            }
+        )
+    else:
+        table.put_item(
+            Item={
+                KEY_NAME: KEY_VALUE,
+                ATTR_NAME: message
+            }
+        )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            ATTR_NAME: message
+        })
     }
